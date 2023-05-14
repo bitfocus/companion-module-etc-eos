93c0ba64579dc94f26017d3b0f3f9cdcd1abb7ce
Julian Waller <git@julusian.co.uk>
2023-05-14 12:08:05 +0100

    feat: convert feedbacks to boolean

--- feedbacks.js
+++ feedbacks.js
@@ -3,21 +3,14 @@ const { combineRgb } = require('@companion-module/base')
 module.exports = async function (self) {
 	self.setFeedbackDefinitions({
 		pending_cue: {
+			type: 'boolean',
 			name: 'When cue is pending',
 			description: "Changes the button's style when this cue is pending.",
+			defaultStyle: {
+				bgcolor: combineRgb(204,102,0),
+				color: combineRgb(255,255,255),
+			},
 			options: [
-				{
-					id: 'bg',
-					type: 'colorpicker',
-					label: 'Background Color',
-					default: combineRgb(204,102,0),
-				},
-				{
-					id: 'fg',
-					type: 'colorpicker',
-					label: 'Foreground Color',
-					default: combineRgb(255,255,255),
-				},
 				{
 					id: 'list',
 					type: 'textinput',
@@ -34,32 +27,19 @@ module.exports = async function (self) {
 				},
 			],
 			callback: (feedback) => {
-				if (feedback.options.list === self.instanceState['cue_pending_list'] &&
-						feedback.options.number === self.instanceState['cue_pending_num']) {
-
-					return {
-						color   : feedback.options.fg,
-						bgcolor : feedback.options.bg,
-					}
-				}
+				return (feedback.options.list === self.instanceState['cue_pending_list'] &&
+						feedback.options.number === self.instanceState['cue_pending_num']) 
 			},
 		},
 		active_cue: {
+			type: 'boolean',
 			name: 'When cue is active',
 			description: "Changes the button's style when this cue is active.",
+			defaultStyle: {
+				bgcolor: combineRgb(51,102,0),
+				color: combineRgb(255,255,255),
+			},
 			options: [
-				{
-					id: 'bg',
-					type: 'colorpicker',
-					label: 'Background Color',
-					default: combineRgb(51,102,0),
-				},
-				{
-					id: 'fg',
-					type: 'colorpicker',
-					label: 'Foreground Color',
-					default: combineRgb(255,255,255),
-				},
 				{
 					id: 'list',
 					type: 'textinput',
@@ -76,32 +56,19 @@ module.exports = async function (self) {
 				},
 			],
 			callback: (feedback) => {
-				if (feedback.options.list === self.instanceState['cue_active_list'] &&
-						feedback.options.number === self.instanceState['cue_active_num']) {
-
-					return {
-						color   : feedback.options.fg,
-						bgcolor : feedback.options.bg,
-					}
-				}
+				return (feedback.options.list === self.instanceState['cue_active_list'] &&
+						feedback.options.number === self.instanceState['cue_active_num']) 
 			},
 		},
-		conneccted: {
+		connected: {
+			type: 'boolean',
 			name: 'When connection to console changes',
 			description: 'Changes colors when the connection state to the console changes.',
+			defaultStyle: {
+				bgcolor: combineRgb(51,102,0),
+				color: combineRgb(255,255,255),
+			},
 			options: [
-				{
-					id: 'bg',
-					type: 'colorpicker',
-					label: 'Background Color',
-					default: combineRgb(51,102,0),
-				},
-				{
-					id: 'fg',
-					type: 'colorpicker',
-					label: 'Foreground Color',
-					default: combineRgb(255,255,255),
-				},
 				{
 					id: 'connected',
 					type: 'checkbox',
@@ -110,18 +77,7 @@ module.exports = async function (self) {
 				},
 			],
 			callback: (feedback) => {
-				if (feedback.type === 'connected') {
-					// The connection to the console changed.
-
-					if (feedback.options.connected === self.instanceState['connected']) {
-
-						return {
-							color   : feedback.options.fg,
-							bgcolor : feedback.options.bg,
-						}
-
-					}
-				}			
+				return (feedback.options.connected === self.instanceState['connected'])
 			},
 		},
 	})
--- upgrades.js
+++ upgrades.js
@@ -1,4 +1,11 @@
+const {CreateConvertToBooleanFeedbackUpgradeScript} = require('@companion-module/base')
+
 module.exports = [
+	CreateConvertToBooleanFeedbackUpgradeScript({
+		pending_cue: true,
+		active_cue: true,
+		connected: true,
+	}),
 	/*
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
