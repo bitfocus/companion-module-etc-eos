9529f93b63da7ff7f0505be069a873f0c2755fc7
Peter Newman <peterjnewman@gmail.com>
2025-08-18 22:57:18 +0100

    Add some tests of the existing parseCueName function

---
+++ main.test.js
@@ -0,0 +1,90 @@
+// Import the necessary modules and classes
+const runEntrypoint = require('@companion-module/base').runEntrypoint
+const { InstanceStatus } = require('@companion-module/base')
+
+// Mock Companion to get the class
+jest.mock('@companion-module/base', () => {
+	const original = jest.requireActual('@companion-module/base')
+	return {
+		...original,
+		InstanceBase: jest.fn(),
+		runEntrypoint: jest.fn(),
+	}
+})
+
+// Define the test suite for ModuleInstance
+describe('ModuleInstance', () => {
+	let instance
+
+	const ModuleInstance = require('./main')
+	const module = runEntrypoint.mock.calls[0][0]
+
+	beforeEach(() => {
+		instance = new module('')
+		instance.instanceState = {}
+		instance.setVariableValues = jest.fn()
+	})
+
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	describe('getConfigFields', () => {
+		test('should return an array of config fields', () => {
+			// Invoke the method
+			instance.config = {
+				supportsManualAdjustments: false,
+			}
+			const configFields = instance.getConfigFields()
+
+			// Assertions
+			expect(Array.isArray(configFields)).toBe(true)
+			expect(configFields.length).toBe(3)
+		})
+	})
+
+	describe('parseCueName', () => {
+		test('should handle an active cue', () => {
+			expect(instance.parseCueName('active', '')).toEqual()
+			expect(instance.setVariableValues).toHaveBeenCalled()
+		})
+
+		test('should handle a pending cue < 1 min', () => {
+			expect(instance.parseCueName('pending', '1/0.91 test 59.0')).toEqual()
+			expect(instance.setVariableValues).toHaveBeenCalledWith({
+				cue_pending_duration: '59.0',
+				cue_pending_intensity: undefined,
+				cue_pending_label: ' test',
+			})
+		})
+
+		test('should handle a cue with level', () => {
+			expect(instance.parseCueName('test', '51.1 Drums 3.0 100%')).toEqual()
+			expect(instance.setVariableValues).toHaveBeenCalledWith({
+				cue_test_duration: '3.0',
+				cue_test_intensity: '100%',
+				cue_test_label: ' Drums',
+			})
+		})
+
+		test('should handle a cue with level but no label', () => {
+			expect(instance.parseCueName('test', '51.1 3.0 100%')).toEqual()
+			expect(instance.setVariableValues).toHaveBeenCalledWith({
+				cue_test_duration: '3.0',
+				cue_test_intensity: '100%',
+				// TODO(Peter): Should be cue number as per code comments
+				cue_test_label: undefined,
+			})
+		})
+
+		test('should handle a cue with no label', () => {
+			expect(instance.parseCueName('test', '51.1 3.0')).toEqual()
+			expect(instance.setVariableValues).toHaveBeenCalledWith({
+				cue_test_duration: '3.0',
+				cue_test_intensity: undefined,
+				// TODO(Peter): Should be cue number as per code comments
+				cue_test_label: undefined,
+			})
+		})
+	})
+})
