f14ce2957b1903b80bd2686ce3ea7dd4717e600c
313ctric <76964455+313ctric@users.noreply.github.com>
2026-05-20 20:26:08 +0100

    Rework Label Polling
    Redo label system to sync all labels from Eos using count and index messages, and request labels for more types of item.
    
    Labels are available as value feedbacks, which allows selecting any label without us having to generate hundreds of variables.
    
    Added some direct-select style actions for each of the item types.
    
    Also fixed a minor issue with the colour hue/saturation when it is empty.

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,59 @@ exports.DEFAULT_NUM_LABELS = 30,
 exports.NUM_SOFTKEYS = 12,
 exports.EOS_PORT = 3032,
 exports.EOS_PORT_SLIP = 3037;
-exports.LABEL_NAMES = ['group', 'preset', 'macro'];
+// this will work for most item types:
+//     cuelist, curve, fpe (fixture position estimation),
+//     pixmap,
+// will not work with:
+//     cue
+//     patch might be possible
+exports.LABEL_NAMES = [
+    'macro',
+    'sub',
+    'preset',
+    'group',
+    'ip', 'fp', 'cp', 'bp',
+    'fx', 'snap', 'ms'
+];
+
+exports.ITEM_COLOURS = {
+    macro:  0x403837,
+    sub:    0x004A26,
+    preset: 0x0A3742,
+    ip:     0x6E2A15,
+    fp:     0x003824,
+    cp:     0x31313D,
+    bp:     0x0E1A4A,
+    group:  0x364757,
+    fx:     0x36154A,
+    snap:   0x621111,
+    ms:     0x570D28,
+    scene:  0x004A26,
+    patch:  0x13202E,
+
+    
+    soft_key: 0x141414,
+    live:     0xC78B07,
+    blind:    0x82B4FF,
+};
+exports.ITEM_NAMES = {
+    macro:  "Macro",
+    sub:    "Submaster",
+    preset: "Preset",
+    ip:     "Intensity Palette",
+    fp:     "Focus Palette",
+    cp:     "Colour Palette",
+    bp:     "Beam Palette",
+    group:  "Group",
+    fx:     "Effect",
+    snap:   "Snapshot",
+    ms:     "Magic Sheet",
+    scene:  "Scene",
+
+    patch:  "Channel",
+    pixmap: "Pixel Map",
+    cuelist:"Cue List",
+    cue:    "Cue",
+    fpe:    "FPE Point",
+    curve:  "Curve",
+};
---
+++ src/sync.js
@@ -0,0 +1,134 @@
+const constants = require('./constants.js')
+
+/**
+ * 
+ * @param {number} param_index 
+ * @param {number} start_index 
+ * @param {{}[]} arguments 
+ * @returns {any|null}
+ */
+function get_list_param(param_index, start_index, arguments) {
+    if (start_index > param_index) return null;
+    let list_index = param_index - start_index;
+    if (list_index >= arguments.length) return null;
+
+    if (arguments[list_index] === undefined) {
+        return "sdfldsjf;asldfjkas;lfdkjas";
+    }
+
+    return arguments[list_index].value;
+}
+
+class EosSync {
+    constructor(parameters) {
+        for (const item_type of constants.LABEL_NAMES) {
+            this[item_type] = new Map();
+        }
+        
+        this.feedback_numbers = new Map();
+    }
+
+    /**
+     * Clear information about an item
+     * 
+     * We only clear the label here, so we keep the subscriptions and can use them when updating later
+     * 
+     * @param {string} item_type 
+     * @param {string} item_num 
+     */
+    set_item_deleted(item_type, item_number) {
+
+        let obj = this[item_type].get(item_number);
+        obj.label = undefined;
+        return obj.subscriptions ?? [];
+    }
+
+    /**
+     * 
+     * @param {string} item_type 
+     * @param {string} item_num 
+     * @param {number} list_start_index 
+     * @param {{}[]} list_values 
+     * @returns {string[]} list of feedback IDs subscribed to this thing
+     */
+    set_item_values(item_type, item_number, list_start_index, list_values) {        
+        let label = get_list_param(2, list_start_index, list_values)
+        if (label) {
+            let subs = this[item_type].get(item_number)?.subscriptions ?? [];
+
+            this[item_type].set(item_number, {
+                label: label,
+                subscriptions: subs
+            });
+
+            return subs;
+        }
+
+        return [];
+    }
+
+    /**
+     * 
+     * @param {string} item_type 
+     * @param {string} item_number 
+     * @param {string} feedback_id 
+     */
+    get_item_value(item_type, item_number, feedback_id) {
+        let prev_feedback_num = this.feedback_numbers.get(feedback_id);
+        if (prev_feedback_num !== undefined) {
+            if (prev_feedback_num != item_number) {
+                this.remove_item_subscription(item_type, prev_feedback_num, feedback_id);
+            }
+        }
+        this.feedback_numbers.set(feedback_id, item_number);
+
+        let item = this[item_type].get(item_number);
+
+        if (item === undefined) {
+            // not synced from Eos, we still store the subscriptions though
+            this[item_type].set(item_number, {
+                subscriptions: [feedback_id]
+            });
+            return '';
+        }
+
+        // register subscription ID
+        if (!item.subscriptions.includes(feedback_id)) {
+            item.subscriptions.push(feedback_id)
+        }
+
+        return item.label ?? '';
+    }
+
+    /**
+     * 
+     * @param {string} item_type 
+     * @param {string} item_number 
+     * @param {string} feedback_id 
+     * @returns 
+     */
+    remove_item_subscription(item_type, item_number, feedback_id) {
+        this.feedback_numbers.delete(feedback_id);
+        let item = this[item_type].get(item_number);
+
+        if (item === undefined) return;
+
+        const index = item.subscriptions.indexOf(feedback_id);
+        if (index > -1) {
+            item.subscriptions.splice(index, 1);
+        }
+    }
+
+    reset() {
+        for (const item_type of constants.LABEL_NAMES) {
+            let items = this[item_type];
+            // this also clears out all of our subscriptions
+            // however they will be re-registered next time the callback runs
+            items.clear();
+        }
+    }
+
+}
+
+module.exports.EosSync = EosSync
+module.exports.get_list_param = get_list_param
